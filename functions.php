370e46f1ef8a2b4a9156a517ab861d6225222429
Amin Zohrabi Far <47514360+aminzohrabifar@users.noreply.github.com>
2019-03-25 10:08:13 +0430

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,57 @@
+remove_action( 'wp_ajax_nopriv_woocommerce_get_refreshed_fragments', array( 'WC_AJAX', 'get_refreshed_fragments' ) );
+remove_action( 'wp_ajax_woocommerce_get_refreshed_fragments', array( 'WC_AJAX', 'get_refreshed_fragments' ) );
+add_action( 'init', 'my_deregister_heartbeat', 1 );
+function my_deregister_heartbeat() {
+global $pagenow;
+
+if ( 'post.php' != $pagenow && 'post-new.php' != $pagenow )
+wp_deregister_script('heartbeat');
+}
+
+add_action( 'wp_print_scripts', 'de_script', 100 );
+
+function de_script() {
+wp_dequeue_script( 'wc-cart-fragments' );
+
+return true;
+}
+
+/** Disable Ajax Call from WooCommerce */
+add_action( 'wp_enqueue_scripts', 'dequeue_woocommerce_cart_fragments', 11); 
+function dequeue_woocommerce_cart_fragments() { if (is_front_page()) wp_dequeue_script('wc-cart-fragments'); }
+
+
+
+/** Disable All WooCommerce  Styles and Scripts Except Shop Pages*/
+add_action( 'wp_enqueue_scripts', 'dequeue_woocommerce_styles_scripts', 99 );
+function dequeue_woocommerce_styles_scripts() {
+if ( function_exists( 'is_woocommerce' ) ) {
+if ( ! is_woocommerce() && ! is_cart() && ! is_checkout() ) {
+# Styles
+wp_dequeue_style( 'woocommerce-general' );
+wp_dequeue_style( 'woocommerce-layout' );
+wp_dequeue_style( 'woocommerce-smallscreen' );
+wp_dequeue_style( 'woocommerce_frontend_styles' );
+wp_dequeue_style( 'woocommerce_fancybox_styles' );
+wp_dequeue_style( 'woocommerce_chosen_styles' );
+wp_dequeue_style( 'woocommerce_prettyPhoto_css' );
+# Scripts
+wp_dequeue_script( 'wc_price_slider' );
+wp_dequeue_script( 'wc-single-product' );
+wp_dequeue_script( 'wc-add-to-cart' );
+wp_dequeue_script( 'wc-cart-fragments' );
+wp_dequeue_script( 'wc-checkout' );
+wp_dequeue_script( 'wc-add-to-cart-variation' );
+wp_dequeue_script( 'wc-single-product' );
+wp_dequeue_script( 'wc-cart' );
+wp_dequeue_script( 'wc-chosen' );
+wp_dequeue_script( 'woocommerce' );
+wp_dequeue_script( 'prettyPhoto' );
+wp_dequeue_script( 'prettyPhoto-init' );
+wp_dequeue_script( 'jquery-blockui' );
+wp_dequeue_script( 'jquery-placeholder' );
+wp_dequeue_script( 'fancybox' );
+wp_dequeue_script( 'jqueryui' );
+}
+}
+}
